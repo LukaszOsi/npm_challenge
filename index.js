81005976583dab624ebcabb938263255a45c127d
Lukasz Ospi <ospi@tlen.pl>
2021-03-15 21:42:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const information = require ('./information.js');
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : myName,
+	e : "oO",
+	T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,3 @@
+const name = "Ospi";
+const campus = "Berlin";
+module.exports = myName = (`Hello my name is ${name} and I learn at ${campus} Campus.`)
